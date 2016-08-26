309b9c11d8dbe4ed98b9ed4028a3d6b7dccadde7
Stephen McGinty <stephen.mcginty@nxp.com>
2016-08-26 03:58:45 -0500

    Everything compiles

--- v93k/src/origen/utils/version.hpp
+++ v93k/src/origen/utils/version.hpp
@@ -1,14 +1,21 @@
 #ifndef ORIGEN_UTILS_VERSION_INCLUDED
 #define ORIGEN_UTILS_VERSION_INCLUDED
 
+#include <string>
+using namespace std;
+
 namespace origen {
 	namespace utils {
+
 		class Version {
 
-		   	Version(std::string version&);
-		   	bool operator<(Version rhs);
+			public:
+
+				Version(std::string& version);
+				bool operator<(Version rhs);
 	   	};
-	};
-};
+
+	}
+}
 
 #endif
