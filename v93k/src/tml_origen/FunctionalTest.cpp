373d7aa71011b667cd86937ce8987d1efec3d144
Stephen McGinty <stephen.mcginty@nxp.com>
2016-08-29 05:30:20 -0500

    Added empty functional test method, all builds via make

---
+++ v93k/src/tml_origen/FunctionalTest.cpp
@@ -0,0 +1,55 @@
+#include "testmethod.hpp"
+
+//for test method API interfaces
+#include "mapi.hpp"
+using namespace std;
+
+/**
+ * Test method class.
+ *
+ * For each testsuite using this test method, one object of this
+ * class is created.
+ */
+class FunctionalTest: public testmethod::TestMethod {
+
+protected:
+  /**
+   *Initialize the parameter interface to the testflow.
+   *This method is called just once after a testsuite is created.
+   */
+  virtual void initialize()
+  {
+    //Add your initialization code here
+    //Note: Test Method API should not be used in this method!
+  }
+
+  /**
+   *This test is invoked per site.
+   */
+  virtual void run()
+  {
+    //Add your test code here.
+    return;
+  }
+
+  /**
+   *This function will be invoked once the specified parameter's value is changed.
+   *@param parameterIdentifier
+   */
+  virtual void postParameterChange(const string& parameterIdentifier)
+  {
+    //Add your code
+    //Note: Test Method API should not be used in this method!
+    return;
+  }
+
+  /**
+   *This function will be invoked once the Select Test Method Dialog is opened.
+   */
+  virtual const string getComment() const 
+  {
+    string comment = " please add your comment for this method.";
+    return comment;
+  }
+};
+REGISTER_TESTMETHOD("FunctionalTest", FunctionalTest);
