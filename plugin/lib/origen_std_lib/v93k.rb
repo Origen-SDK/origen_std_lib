b364f915f8459815b7793e9efcf3243d9aa1ecd3
Stephen McGinty <stephen.mcginty@nxp.com>
2017-02-09 03:35:37 -0600

    Added delayed binning helpers

--- plugin/lib/origen_std_lib/v93k.rb
+++ plugin/lib/origen_std_lib/v93k.rb
@@ -42,6 +42,40 @@ module OrigenStdLib
                 pin:          [:string, nil],
                 samples:      [:integer, 2000],
                 period_in_ns: [:integer, nil]
+              },
+
+              record_bin: {
+                bin: [:integer, nil],
+                softbin: [:integer, nil]
+              },
+
+              apply_bin: {
               }
+
+  end
+
+  # Record the given bin / softbin
+  def record_bin(options = {})
+    if tester.v93k?
+      n = "delayed"
+      n += "_bin#{options[:bin]}" if options[:bin]
+      n += "_sbin#{options[:softbin]}" if options[:softbin]
+      i = test_suites.add(n)
+      i.test_method = test_methods.origen.record_bin(options)
+      # The bins are included here to insert them into the flow, which is required by SMT to
+      # allow them to be applied later.
+      # However the bins really refer to some parent test, so we tell the TestIds plugin (if
+      # it is being used), not to consider them attached to this record bin test suite.
+      test(i, options.merge(test_ids: :notrack))
+    end
+  end
+  
+  # Bins out the device if a delayed bin has been set
+  def apply_bin(options = {})
+    if tester.v93k?
+      i = test_suites.add("ApplyBin")
+      i.test_method = test_methods.origen.apply_bin()
+      test(i, options.merge(on_fail: { render: "multi_bin;" }, continue: true))
+    end
   end
 end
